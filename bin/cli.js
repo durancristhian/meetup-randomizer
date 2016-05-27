ca15d9bb55f4df7b31b3832e7539a7b651c7b169
Cristhian Duran <durancristhian@gmail.com>
2016-05-27 19:50:22 -0300

    Exclude --api-key parameter

--- bin/cli.js
+++ bin/cli.js
@@ -8,27 +8,18 @@ const pkg = require('../package.json')
 
 commander
   .version(pkg.version)
-  .usage('--meetup-name [MEETUP_NAME] --event-id [EVENT_ID] --api-key [API_KEY]')
+  .usage('--meetup-name [MEETUP_NAME] --event-id [EVENT_ID]')
   .option('--meetup-name [MEETUP_NAME]', `Meetup's name`) // eslint-disable-line quotes
   .option('--event-id [EVENT_ID]', `Event's id`) // eslint-disable-line quotes
-  .option('--api-key [API_KEY]', 'Your api key')
   .parse(process.argv)
 
-if (!commander.meetupName || !commander.eventId || !commander.apiKey) {
-  console.log('')
-  console.error('There is a missing parameter')
+if (!commander.meetupName || !commander.eventId) commander.help()
 
-  commander.help()
-}
-
-program(commander.meetupName, commander.eventId, commander.apiKey)
+program(commander.meetupName, commander.eventId)
   .then(() => {
     process.exit(0)
   })
   .catch(error => {
-    console.log('')
     console.error(error)
-    console.log('')
-
-    process.exit(1)
+    process.exit(error)
   })
