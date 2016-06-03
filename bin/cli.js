7f35c07ac587f0d0611c0f16e17c1ca3795765f4
Cristhian Duran <durancristhian@gmail.com>
2016-06-02 21:10:03 -0300

    fix(cli): Required parameters
    It updates the commander syntax to ensure that the arguments are valid

--- bin/cli.js
+++ bin/cli.js
@@ -9,8 +9,8 @@ const pkg = require('../package.json')
 commander
   .version(pkg.version)
   .usage('--meetup-name [MEETUP_NAME] --event-id [EVENT_ID]')
-  .option('--meetup-name [MEETUP_NAME]', `Meetup's name`) // eslint-disable-line quotes
-  .option('--event-id [EVENT_ID]', `Event's id`) // eslint-disable-line quotes
+  .option('--meetup-name <MEETUP_NAME>', `Meetup's name`) // eslint-disable-line quotes
+  .option('--event-id <EVENT_ID>', `Event's id`) // eslint-disable-line quotes
   .parse(process.argv)
 
 if (!commander.meetupName || !commander.eventId) commander.help()
