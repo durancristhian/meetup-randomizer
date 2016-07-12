9c1c6da805ff6a006d565f31dace4295fc716bec
Alejandro Oviedo Garcia <Alejandro.Oviedo.Garcia-Contractor@adp.com>
2016-07-12 10:58:40 -0300

    feat(cli): add url option

--- bin/cli.js
+++ bin/cli.js
@@ -5,15 +5,24 @@ const program = require('../lib/program')
 const showWinnerImage = require('../lib/modules/show-winner-image')
 const API_URL = process.env.API_URL
 const PROFILE_URL = process.env.PROFILE_URL
+const url = require('url')
 
 commander
-  .usage('--meetup-name [MEETUP_NAME] --event-id [EVENT_ID]')
+  .usage('--url [EVENT_URL] --meetup-name [MEETUP_NAME] --event-id [EVENT_ID]')
   .option('-m, --meetup-name <MEETUP_NAME>', `meetup's name. For example: banodejs`) // eslint-disable-line quotes
   .option('-e, --event-id <EVENT_ID>', `event's id. For example: 231888421`) // eslint-disable-line quotes
+  .option('-u, --url <EVENT_URL>', `event's url. For example:`) // eslint-disable-line quotes
   .parse(process.argv)
 
-if (!commander.meetupName || !commander.eventId) {
+if (!commander.url && (!commander.meetupName || !commander.eventId)) {
   commander.help()
+}
+
+if (commander.url) {
+  const path = url.parse(commander.url).path
+  const pathSlices = path.split('/')
+  commander.eventId = pathSlices[pathSlices.length - 2]
+  commander.meetupName = pathSlices[pathSlices.length - 4]
 }
 
 program(API_URL, PROFILE_URL, commander.meetupName, commander.eventId)
